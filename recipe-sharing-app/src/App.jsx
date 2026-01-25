7ebcc9882b401662453d664d596a6ec04a65c6be
Ashraf Magdy <ashrafmagdy926@gmail.com>
2026-01-25 22:09:01 +0200

    updated tasks

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+import SearchBar from "./components/SearchBar";
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
 import RecipeDetailsPage from "./pages/RecipeDetailsPage";
+
+const Home = () => {
+  return (
+    <div style={{ maxWidth: 720, margin: "0 auto", padding: 16 }}>
+      <h1 style={{ marginTop: 0 }}>Recipe Sharing App 🍲</h1>
+
+      <AddRecipeForm />
+      <SearchBar />
+      <RecipeList />
+
+      <hr style={{ margin: "18px 0" }} />
+      <FavoritesList />
+      <RecommendationsList />
+    </div>
+  );
+};
 
 function App() {
   return (
