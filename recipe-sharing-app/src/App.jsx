937ece25b6f68c4f0555c93f5443b58a688820a2
Ashraf Magdy <ashrafmagdy926@gmail.com>
2026-01-25 22:17:43 +0200

    checker fix

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,24 +1,44 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
+import SearchBar from "./components/SearchBar";
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
 import RecipeDetails from "./components/RecipeDetails";
+
+const Home = () => {
+  return (
+    <div style={{ maxWidth: 720, margin: "0 auto", padding: 16 }}>
+      <h1 style={{ marginTop: 0 }}>Recipe Sharing App 🍲</h1>
+
+      <AddRecipeForm />
+      <SearchBar />
+      <RecipeList />
+
+      <hr style={{ margin: "18px 0" }} />
+      <FavoritesList />
+      <RecommendationsList />
+    </div>
+  );
+};
+
+const RecipeDetailsRoute = () => {
+  return (
+    <div style={{ maxWidth: 720, margin: "0 auto", padding: 16 }}>
+      <Link to="/">← Back</Link>
+      <RecipeDetails />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <AddRecipeForm />
-              <RecipeList />
-            </div>
-          }
-        />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes/:id" element={<RecipeDetailsRoute />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
